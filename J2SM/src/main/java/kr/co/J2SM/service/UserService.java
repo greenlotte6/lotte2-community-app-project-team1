7f28b6cdb8c22d8c636d5100ff9792fc3a405411
GreenPai <clsrntkdgh@gmail.com>
2025-06-09 13:55:31 +0900

    채팅

--- J2SM/src/main/java/kr/co/J2SM/service/UserService.java
+++ J2SM/src/main/java/kr/co/J2SM/service/UserService.java
@@ -176,4 +176,6 @@ public class UserService{
         }
         return false;
     }
+
+
 }
--- React/J2SM/src/contexts/AuthContext.jsx
+++ React/J2SM/src/contexts/AuthContext.jsx
@@ -6,32 +6,52 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   // Context에서 사용하는 상태값
   const [username, setUsername] = useState(null);
+  const [department, setDepartment] = useState(null);
+  const [company, setCompany] = useState(null);
 
   useEffect(() => {
     // 쿠키에서 username 가져오기
     const username = Cookies.get("username");
+    const department = Cookies.get("department");
+    const company = Cookies.get("company");
 
     // 쿠키값이 존재하면 Context username 초기화
     if (username) {
       setUsername(username);
     }
+
+    if (department) {
+      setDepartment(department);
+    }
+
+    if (company) {
+      setCompany(company);
+    }
   }, []);
 
-  const login = (username) => {
+  const login = (username, department, company) => {
     // Context username 초기화
     setUsername(username);
+    setDepartment(department);
+    setCompany(company);
 
     // 쿠키 저장
     Cookies.set("username", username);
+    Cookies.set("department", department);
+    Cookies.set("company", company);
   };
 
   const logout = () => {
     setUsername(null);
     Cookies.remove("username");
+    Cookies.remove("department");
+    Cookies.remove("company");
   };
 
   return (
-    <AuthContext.Provider value={{ username, login, logout }}>
+    <AuthContext.Provider
+      value={{ username, company, department, login, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
