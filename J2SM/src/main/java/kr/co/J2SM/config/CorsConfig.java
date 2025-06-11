f40db2c342e63630bb213e57293a2fc3782d4021
GreenPai <clsrntkdgh@gmail.com>
2025-06-11 11:57:47 +0900

    mix 테스트

--- J2SM/src/main/java/kr/co/J2SM/config/CorsConfig.java
+++ J2SM/src/main/java/kr/co/J2SM/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/**")        // 모든 엔드포인트에 대한 접근허용
                 //.allowedOriginPatterns("http://127.0.0.1:5173", "http://localhost:5173", "http://localhost:5174", "https://j2sm-g4rr2iziz-greenlotte6s-projects.vercel.app")             // 해당 도메인 origin의 요청 허용
-                .allowedOriginPatterns("*")             // 해당 도메인 origin의 요청 허용
+                .allowedOriginPatterns("*", "https://lotte2-community-app-project-t-git-bc5935-greenlotte6s-projects.vercel.app/user/login")             // 해당 도메인 origin의 요청 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 Methods 설정
                 .allowedHeaders("*")        // 모든 헤더 정보 허용
                 .allowCredentials(true)     // 자격 증명 허용
