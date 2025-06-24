06298e8a7370b7ba4651291ecc8a3465e7a889f8
wnstj050505 <wnstj050505@gmail.com>
2025-06-24 15:38:10 +0900

    deploysibalment

--- J2SM/src/main/java/kr/co/J2SM/config/CorsConfig.java
+++ J2SM/src/main/java/kr/co/J2SM/config/CorsConfig.java
@@ -21,7 +21,10 @@ public class CorsConfig implements WebMvcConfigurer {
                 //        "https://lotte2-community-app-project-team1-sandy.vercel.app",
                 //        "http://localhost:5173"
                 //)
-                .allowedOrigins("https://www.j2sm.site")
+                .allowedOrigins(
+                        "https://www.j2sm.site",
+                        "https://lotte2-community-app-project-team1-sandy.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 Methods 설정
                 .allowedHeaders("*")        // 모든 헤더 정보 허용
                 .allowCredentials(true)     // 자격 증명 허용
