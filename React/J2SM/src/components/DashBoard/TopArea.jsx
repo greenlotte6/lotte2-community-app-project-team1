b6168b304237420f4194ee82e0a1f102e8fa93d6
GreenPai <clsrntkdgh@gmail.com>
2025-06-26 12:53:14 +0900

    수정

--- React/J2SM/src/components/DashBoard/TopArea.jsx
+++ React/J2SM/src/components/DashBoard/TopArea.jsx
@@ -8,7 +8,7 @@ const TopArea = () => {
   const [user, setUser] = useState(null);
   const [currentTime, setCurrentTime] = useState("--:--:--");
   const [statusMessage, setStatusMessage] = useState("");
-  const { username } = useAuth();
+  const { username, nick, company } = useAuth();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,6 +19,44 @@ const TopArea = () => {
   }, []);
 
   // 유저 정보 들고오기
+
+  // 소셜 로그인 토큰
+
+  const { login } = useAuth();
+
+  useEffect(() => {
+    if (nick !== null && username !== null && company !== null) {
+      return;
+    }
+
+    axios
+      .get("https://api.j2sm.site/api/user/me", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log("소셜 인증됨 ", res.data);
+        const data = res.data;
+        console.log(res.data.uid);
+        console.log(res.data.name);
+
+        if (res.data.uid) {
+          // context login 호출
+          login(
+            res.data.uid,
+            decodeURIComponent(res.data.department),
+            decodeURIComponent(res.data.company),
+            res.data.name,
+            res.data.membership
+          );
+
+          // 메인 이동(컴포넌트 라우팅)
+          // navigate("/dashboard/main");
+        }
+      })
+      .catch((err) => {
+        console.error("인증 실패 또는 쿠키 없음", err);
+      });
+  }, []);
 
   useEffect(() => {
     const fetchUser = async () => {
